6da4223d9e62043436983ddedf3b0a9b53947a9d
Aleksandr Plakushchyi <alex2808pl@gmail.com>
2023-09-01 12:56:48 +0300

    lesson2

---
+++ src/main/java/de/telran/lesson2/ParseNumber.java
@@ -0,0 +1,10 @@
+package de.telran.lesson2;
+
+public class ParseNumber {
+    public static void main(String[] args) {
+        int myNum = 345;
+        System.out.println(myNum % 10);
+        System.out.println(myNum / 10 % 10);
+        System.out.println(myNum / 100);
+    }
+}
